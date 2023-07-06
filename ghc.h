2a2d5a08d3e12376cf20360057bcb10b326b5f3a
tadashi9e <tadashi9@gmail.com>
2023-07-07 06:04:12 +0900

    divide return stack from register stack

--- ghc.h
+++ ghc.h
@@ -184,27 +184,55 @@ namespace reg {
   };
 }  // namespace reg
 
-#define INITIAL_REG_SIZE 4096
+#define INITIAL_REG_SIZE 32
 #define HEAP_SIZE 4096
+
+class HeapList {
+ public:
+  using RawHeap = std::shared_ptr<std::array<A, HEAP_SIZE> >;
+  static HeapList& getInstance();
+  RawHeap allocate() {
+    RawHeap r = std::make_shared<std::array<A, HEAP_SIZE> >();
+    for (size_t i = 0; i < HEAP_SIZE; ++i) {
+      std::atomic_init(&(*r)[i], static_cast<Q>(0));
+    }
+    heaps_.push_back(r);
+    return r;
+  }
+ private:
+  std::vector<RawHeap> heaps_;
+};
+
+HeapList& HeapList::getInstance() {
+  static HeapList heap_list_;
+  return heap_list_;
+}
 
 class Heap {
  public:
   Heap() {
-    for (size_t i = 0; i < HEAP_SIZE; ++i) {
-      std::atomic_init(&heap_[i], static_cast<Q>(0));
-    }
+    heap_ = HeapList::getInstance().allocate();
+  }
+  void fresh_heap() {
+    heap_ = HeapList::getInstance().allocate();
   }
   A& operator[](size_t index) {
-    return heap_[index];
+    if (index >= HEAP_SIZE) {
+      throw new std::runtime_error("out of heap");
+    }
+    return (*heap_)[index];
   }
   const A& operator[](size_t index) const {
-    return heap_[index];
+    if (index >= HEAP_SIZE) {
+      throw new std::runtime_error("out of heap");
+    }
+    return (*heap_)[index];
   }
   size_t size() const {
     return HEAP_SIZE;
   }
  private:
-  std::array<A, HEAP_SIZE> heap_;
+  std::shared_ptr<std::array<A, HEAP_SIZE> > heap_;
 };
 
 struct VM;
@@ -226,9 +254,16 @@ Scheduler& Scheduler::getInstance() {
 }
 
 struct Context {
-  Context* prev;
+  Context()
+    : in_offset(0),
+      pc_continue(-1),
+      pc_on_error(-1),
+      push_pos(0) {
+  }
+  size_t in_offset;
   int pc_continue;
   int pc_on_error;
+  size_t push_pos;
 };
 
 enum LOG_LEVEL {
@@ -262,22 +297,20 @@ struct VM {
   bool failed;
   int pc;
   std::vector<Q> reg;
-  Context* context;
   Q* in;
   Q* out;
-  Context* next_context;
+  std::vector<Context> contexts;
+  Context next_context;
   size_t required;
-  size_t push_pos;
   std::vector<Q> wait_list;
   Heap heap;
   size_t published;
   std::shared_ptr<VM> child;
-  VM() : log_level(TRACE), failed(false), next_context(NULL),
-         required(0), push_pos(0), published(0) {
+  VM() : log_level(TRACE), failed(false),
+         required(0), published(0) {
     reg.resize(INITIAL_REG_SIZE);
-    context = reinterpret_cast<Context*>(&reg[0]);
-    context->prev = NULL;
-    in = reinterpret_cast<Q*>(context + 1);
+    contexts.push_back(Context());
+    in = &reg[0];
     out = in;
   }
   bool is_log_debug() const {
@@ -291,16 +324,17 @@ struct VM {
       return;
     }
     std::cerr << "--------------------" << std::endl;
-    std::cerr << "[depth] continue : on_error : goal" << std::endl;
+    std::cerr << "[depth] continue : on_error : (reg) Goal" << std::endl;
     int depth = 0;
-    for (Context* cp = context; cp != NULL; cp = cp->prev) {
-      Q* in0 = reinterpret_cast<Q*>(cp + 1);
+    for (const Context& context : contexts) {
+      const Q* in0 = &reg[context.in_offset];
       Q goal = *in0;
       int arity = atom_arity_of(goal);
       std::stringstream ss;
       std::cerr << "[" << depth << "] "
-                << cp->pc_continue << " : " << cp->pc_on_error
-                << " : " << atom_str_of(goal) << "/" << arity
+                << context.pc_continue << " : " << context.pc_on_error
+                << " : (" << context.in_offset << ")"
+                << atom_str_of(goal) << "/" << arity
                 << "(";
       ++depth;
       for (int i = 1; i <= arity; ++i) {
@@ -334,51 +368,48 @@ struct VM {
     child.reset();
   }
   void create_new_window(size_t n) {
-    next_context = reinterpret_cast<Context*>(&in[n]);
-    out = reinterpret_cast<Q*>(next_context + 1);
-    if (out - &reg[0] > reg.size() + 64) {
+    out = &in[n];
+    next_context.in_offset = out - &reg[0];
+    while (out - &reg[0] + 64 > reg.size()) {
       reg.resize(reg.size() * 2);
-      next_context = reinterpret_cast<Context*>(&in[n]);
-      out = reinterpret_cast<Q*>(next_context + 1);
+      in = &reg[contexts.back().in_offset];
+      out = &reg[next_context.in_offset];
     }
   }
   void switch_next_window(int target, int return_pc) {
-    next_context->prev = context;
-    next_context->pc_continue = return_pc;
-    next_context->pc_on_error = -1;
-    context = next_context;
+    next_context.pc_continue = return_pc;
+    next_context.pc_on_error = -1;
     in = out;
     pc = target;
-    in[0] = 0;
+    contexts.push_back(next_context);
   }
   bool switch_prev_window() {
-    if (context->prev == NULL) {
+    if (contexts.size() <= 1) {
       pc = -1;
       return false;
     }
-    pc = context->pc_continue;
-    context = context->prev;
-    in = reinterpret_cast<Q*>(context + 1);
+    Context& context = contexts.back();
+    pc = context.pc_continue;
+    contexts.pop_back();
+    in = &reg[contexts.back().in_offset];
     return true;
   }
   void require(int n) {
-    while (&in[n] > &reg.back()) {
+    while (&in[contexts.back().in_offset + n] > &reg.back()) {
       const size_t sz = reg.size();
       reg.resize(sz * 2);
       log_info(this, "resize register: " <<
                sz << " to " << (2 * sz));
+      in = &reg[contexts.back().in_offset];
+      out = in;
     }
-    n += sizeof(Context);
     required = n;
-    push_pos = n;
-  }
-  void new_heap() {
   }
   size_t heap_publishing(size_t sz) {
     if (published + sz < heap.size()) {
       return published;
     }
-    new_heap();
+    heap.fresh_heap();
     published = 0;
     assert(published + sz < heap.size());
     return published;
@@ -391,10 +422,10 @@ struct VM {
     wait_list.clear();
   }
   void push(Q q) {
-    in[++push_pos] = q;
+    in[++contexts.back().push_pos] = q;
   }
   Q pop() {
-    return in[push_pos--];
+    return in[contexts.back().push_pos--];
   }
   bool check_value_if(TAG_T tag, Q q) {
     const TAG_T t = tag_of(q);
@@ -494,7 +525,7 @@ struct VM {
     wait_list.push_back(q);
   }
   void fail() {
-    pc = context->pc_on_error;
+    pc = contexts.back().pc_on_error;
     if (pc != -1) {
       return;
     }
@@ -503,7 +534,7 @@ struct VM {
         pc = -1;
         return;
       }
-      pc = context->pc_on_error;
+      pc = contexts.back().pc_on_error;
       if (pc != -1) {
         return;
       }
@@ -590,7 +621,8 @@ class RuntimeError: public std::runtime_error {
 
 #define MACRO_requires(n)                                 \
   log_trace(vm, vm->pc << ": requires(" << n << ")");     \
-  vm->require(n)
+  vm->require(n);                                         \
+  vm->contexts.back().push_pos = n + 1
 #define MACRO_fail vm->fail(); continue
 #define MACRO_wait(Ai)                                \
   log_trace(vm, vm->pc << ": wait(" << Ai << ")");    \
@@ -625,13 +657,13 @@ class RuntimeError: public std::runtime_error {
 
 #define MACRO_try_guard_else(jump_to)                                   \
   log_trace(vm, vm->pc << ": try_guard_else(" << (jump_to) << ")");     \
-  vm->context->pc_on_error = (jump_to)
+  vm->contexts.back().pc_on_error = (jump_to)
 #define MACRO_try_guard_else_suspend                       \
   log_trace(vm, vm->pc << ": try_guard_else_suspend");     \
-  vm->context->pc_on_error = -1
+  vm->contexts.back().pc_on_error = -1
 #define MACRO_trust_me                      \
   log_trace(vm, vm->pc << ": trust_me");    \
-  vm->context->pc_on_error = -2
+  vm->contexts.back().pc_on_error = -2
 
 // call 呼び出しに備えて次レジスタウィンドウをセットアップする。
 // out レジスタ位置を次レジスタウィンドウ上に設定する。
@@ -682,6 +714,7 @@ class RuntimeError: public std::runtime_error {
 #define MACRO_proceed                           \
   log_trace(vm, vm->pc << ": proceed");         \
   vm->proceed();                                \
+  vm->dump();                                   \
   continue
 #define MACRO_return                            \
   log_trace(vm, vm->pc << ": return");          \
@@ -886,7 +919,7 @@ class RuntimeError: public std::runtime_error {
   throw std::runtime_error("fix me: check_constant")
 #define MACRO_check_nil(Ai)                                 \
   log_trace(vm, vm->pc << ": check_nil(" << Ai << ")");     \
-  if (!check_if(TAG_NIL, Ai)) {                             \
+  if (!vm->check_if(TAG_NIL, Ai)) {                         \
     continue;                                               \
   }
 #define MACRO_check_list(Ai)                      \
